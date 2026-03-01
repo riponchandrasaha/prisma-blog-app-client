bf5eb169d9c4624a6d485d567a6aad37e973f512
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-03-01 22:57:50 +0600

    Making env type safe with proper validation

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { userService } from "@/services/user.service";
+
+
 
 
 export default async function Home() {
- 
+ const {data} = await userService.getSession();
+ console.log(data);
   return (
     <div>
       <Button>click</Button>
---
+++ src/services/user.service.ts
@@ -0,0 +1,31 @@
+import { env } from "@/env";
+import { cookies } from "next/headers";
+const AUTH_URL = env.AUTH_URL;
+
+
+export const userService = {
+    getSession: async function () {
+        try {
+            const cookieStore = await cookies();
+            console.log(cookieStore.toString());
+
+            const res = await fetch(`${AUTH_URL}/get-session`, {
+                headers: {
+                    Cookie: cookieStore.toString(),
+                },
+                cache: "no-store",
+            });
+            const session = await res.json();
+            if (session == null) {
+                return { data: null, error: { message: "session is messing" } }
+
+            }
+            return { data: session, error: null };
+
+        } catch (err) {
+            console.error(err);
+            return { data: null, error: { message: "something went wrong" } };
+
+        }
+    },
+}

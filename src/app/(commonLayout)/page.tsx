f5d32eff597f38c5183582ee8cae83e7000b86b3
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-03-02 23:08:37 +0600

    the purpose of generateStaticParams

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,15 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
+import BlogCard from "@/components/modules/homepage/BlogCard";
 
-
-
+import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
 
 export default async function Home() {
- const {data} = await userService.getSession();
- console.log(data);
+  const { data } = await blogService.getBlogPosts(
+    {
+      isFeatured: false,
+    },
+    {
+      cache: "no-store",
+    }
+  );
+
+  console.log(data);
+
   return (
-    <div>
-      <Button>click</Button>
+    <div className="grid grid-cols-3 max-w-7xl mx-auto px-4 gap-6">
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
